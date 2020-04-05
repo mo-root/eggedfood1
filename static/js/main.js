cf26b9a3109e2edaf3c3c31aae7c621ba1296b5d
mo-root <59801139+mo-root@users.noreply.github.com>
2020-04-05 16:06:41 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,37 @@
+
+  function reg(){
+    window.location = 'log.html'
+  }
+
+function cords() {
+	location.replace("cords/cords.html");
+}
+
+function elc(){
+
+	location.replace("elc/elc.html");
+}
+
+function acc(){
+	location.replace("acc/acc.html");
+}
+
+function food(){
+	location.replace("food/food.html");
+}
+
+
+function pre(){
+	location.replace(".html");
+}
+
+
+function ran(){
+
+
+
+let x=Math.floor(Math.random() * 11);
+if (x>5) {document.getElementById('img1').src = "/home/student/Desktop/meet stuff/meet folder/final_css_pro/web/static/images/ama.png"
+}
+}
+
---
+++ static/js/t.js
@@ -0,0 +1,22 @@
+
+// let x = document.getElementById('G_G');
+// let p = document.getElementById('G_P');
+
+// function Gacc(){
+// let x = document.getElementById('G_G');
+// let p = document.getElementById('G_P');
+
+
+// if ((x == "main.account@gmail.com" && p == "main123")) {
+// window.location = 'main.html';
+
+// }
+// }
+
+
+
+function my() {
+ if( document.getElementById("G_G").value == "main@gmail.com")
+ { if( document.getElementById("G_P").value == "main123") {alert("asdas")}
+ }}
+ 
